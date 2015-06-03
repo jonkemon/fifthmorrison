48712002a5fd64bca6d6d3304c929564140ee46c
John Morrison <john.morrison@turner.com>
2015-06-03 19:07:07 +0100

    tidy up

--- wp-content/themes/twentyfifteen/config.rb
+++
@@ -1,9 +0,0 @@
-syntax = :scss
-http_path = '/'
-css_dir = 'css'
-sass_dir = 'sass'
-images_dir = 'images'
-javascripts_dir = 'js'
-relative_assets = true
-line_comments = true
-# output_style = :compressed
