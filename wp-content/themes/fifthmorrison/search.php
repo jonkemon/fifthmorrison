0189809dab27db368a5ba03d9305d90e99d4e903
John Morrison <john.morrison@turner.com>
2015-06-03 19:04:56 +0100

    theme naming change

--- wp-content/themes/fifthmorrison/search.php
+++ wp-content/themes/fifthmorrison/search.php
@@ -3,8 +3,8 @@
  * The template for displaying search results pages.
  *
  * @package WordPress
- * @subpackage Twenty_Fifteen
- * @since Twenty Fifteen 1.0
+ * @subpackage Fifth_Morrison
+ * @since Fifth Morrison 1.0
  */
 
 get_header(); ?>
@@ -15,7 +15,7 @@ get_header(); ?>
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
-				<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'twentyfifteen' ), get_search_query() ); ?></h1>
+				<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'fifthmorrison' ), get_search_query() ); ?></h1>
 			</header><!-- .page-header -->
 
 			<?php
@@ -35,9 +35,9 @@ get_header(); ?>
 
 			// Previous/next page navigation.
 			the_posts_pagination( array(
-				'prev_text'          => __( 'Previous page', 'twentyfifteen' ),
-				'next_text'          => __( 'Next page', 'twentyfifteen' ),
-				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>',
+				'prev_text'          => __( 'Previous page', 'fifthmorrison' ),
+				'next_text'          => __( 'Next page', 'fifthmorrison' ),
+				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'fifthmorrison' ) . ' </span>',
 			) );
 
 		// If no content, include the "No posts found" template.
